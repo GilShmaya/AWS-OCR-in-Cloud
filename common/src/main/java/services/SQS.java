a5a61021d2c556e4f3ff9ed716a1dc279f55d378
gilshmaya <t-gilshmay@microsoft.com>
2022-11-11 15:35:30 +0200

    rearrange modules

--- common/src/main/java/services/SQS.java
+++ common/src/main/java/services/SQS.java
@@ -1,27 +1,54 @@
 package services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SQS {
+    private static Logger logger = LoggerFactory.getLogger(SQS.class);
     private final Region REGION = Region.US_EAST_1; // TODO: check the region
     private String queueName;
     private SqsClient sqsClient;
+    private Map<QueueAttributeName,String> attributes;
+    private String url;
 
     public SQS(){
         this.sqsClient = SqsClient.builder().region(REGION).build();
+        this.attributes = new HashMap<>();
+        attributes.put(QueueAttributeName.VISIBILITY_TIMEOUT, "60");
     }
-    private void create(String queueName) {
+
+    public void create(String queueName) {
+        if(isActive()) {
+            logger.error("The SQS is already active");
+        }
         this.queueName = queueName;
         CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
                 .queueName(queueName)
+                .attributes(attributes)
                 .build();
 
         sqsClient.createQueue(createQueueRequest);
+        getURL();
     }
 
-    public void getURL() {
+    private boolean isActive(){
+        return url != null;
+    }
+
+    public String getURL() {
+        if(url == null){
+            GetQueueUrlRequest req= GetQueueUrlRequest.builder().queueName(queueName).build();
+            url = sqsClient.getQueueUrl(req).queueUrl();
+        }
+        return url;
     }
 
 
